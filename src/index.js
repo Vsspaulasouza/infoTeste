9869db995e0b20eb540f7e4cb4494263112c8678
Vinícius Soares de Paula Souza <69551648+Vsspaulasouza@users.noreply.github.com>
2022-02-26 16:46:08 -0300

    Terminando banners  e header

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const header = document.querySelector(".header");
+
+function debounce(func, wait, immediate) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function handleScroll() {
+  if (window.scrollY === 0) header.classList.remove("shadow");
+  else header.classList.add("shadow");
+}
+
+window.addEventListener("scroll", debounce(handleScroll, 20));
